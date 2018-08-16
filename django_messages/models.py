a6f46b540938139b7b5d26146c9d3b5fb81c0e23
Charles Thompson <thompsoncharles@live.com>
2018-08-16 14:30:14 +0000

    fix(models): get_absolute_url uses reverse view function name

--- django_messages/models.py
+++ django_messages/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.db.models import signals
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
@@ -81,9 +82,7 @@ class Message(models.Model):
         return self.subject
 
     def get_absolute_url(self):
-        return ('messages_detail', [self.id])
-
-    # get_absolute_url = models.permalink(get_absolute_url)
+        return reverse('messages_detail', args=(self.id,))
 
     def save(self, **kwargs):
         if not self.id:
